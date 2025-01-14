268dc963a2b160fc605a6a9c24f667c2e54cb74c
bilaavs <nabilaramadhani796@gmail.com>
2025-01-14 17:07:42 +0700

    tugaslaravel

---
+++ belajar-laravel/routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/index', function () {
+    return view('index');
+});
+Route::get('/form', function () {
+    return view('form');
+});
+Route::get('/datang', function () {
+    return view('datang');
+});
+Route::get('/welcome', function () {
+    return view('welcome');
+});
+Route::get('/', function () {
+    return view('welcome');
+});
